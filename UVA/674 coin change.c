b0c04954764f9482ead449be6a4b32ffd1a1bbc3
Rana53 <ranajahid.cseru@gmail.com>
2019-07-10 21:44:12 +0600

    uva 674 coin change solved

--- UVA/674 coin change.c
+++ UVA/674 coin change.c
@@ -1,13 +1,29 @@
 #include<stdio.h>
+#include<string.h>
 #define MX 6
 int coin [] = {50, 25, 10, 5, 1};
 int make;
 int dp[MX][7490];
-int knapsack(){
 
+int knapsack(int i , int amount){
+    if(i >= 5)
+        return amount == make;
+    if(dp[i][amount] != -1) return dp[i][amount];
+    int ret1, ret2;
+    ret1 = 0;
+    ret2 = 0;
+    if(amount + coin[i] <= make)
+        ret1 = knapsack(i, amount + coin[i]);
+    ret2 = knapsack(i+1,amount);
+    return dp[i][amount] = ret1 + ret2;
 }
 
 int main(){
-    int
+    while(scanf("%d",&make) != EOF){
+        memset(dp, -1, sizeof(dp));
+        long long int count;
+        count = knapsack(0,0);
+        printf("%d\n",count);
+    }
     return 0;
 }
