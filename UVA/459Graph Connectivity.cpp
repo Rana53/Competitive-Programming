e5fd0c68d4292c7907ccc226ae59c32bd21a308a
Rana53 <ranajahid.cseru@gmail.com>
2019-08-11 23:14:00 +0600

    UVA 459 solution added but can't solved properly

--- UVA/459Graph Connectivity.cpp
+++ UVA/459Graph Connectivity.cpp
@@ -1,26 +1,67 @@
 #include<iostream>
 #include<cstdio>
 #include<cstring>
-
+#define MX 26
 using namespace std;
-int graph[27][27];
-int sz[27];
+int graph[MX][MX];
+int sz[MX];
+int visited[MX];
+
+void DFS(int n){
+    visited[n] = 1;
+    for(int i = 0; i < sz[n];i++)
+        if(!visited[graph[n][i]])
+            DFS(graph[n][i]);
+    return;
+}
 
 int main(){
     int n;
-    char start, input[2], ch;
+    char start, input[2], ch, u, v;
     cin >> n;
     while(n--){
         memset(graph,0,sizeof(graph));
         memset(sz,0,sizeof(sz));
+        memset(visited,0,sizeof(visited));
+
         cin >> start;
         cin.get();
-        cout << "start inpu - " << start << endl;
+        //cout << "First character inputed" << endl;
+        //cout << "start inpu - " << start << endl;
         while(scanf("%[^\n]",input)){
+            cin.get();
+            //cout << " input graph";
             if(input[0] == '\n') break;
-            cout <<" " -  input << endl;
+            u = input[0]-'A';
+            v = input[1]-'A';
+            graph[u][sz[u]++] = v;
+            graph[v][sz[v]++] = u;
         }
-        cout << "input end" << endl;
+       // cout << "Graph input end" << endl;
+        u = start - 'A';
+        if(!sz[u]){
+            graph[u][sz[u]++] = u;
+        }
+        int count = 0;
+        for(int i = 0; i < MX; i++){
+            if(!visited[i] && sz[i]){
+                count++;
+                DFS(i);
+            }
+        }
+        cout << count << endl;
+        if(n) cout << endl;
     }
-
+    return 0;
 }
+
+/*
+1
+
+
+E
+AB
+CE
+DB
+EC
+*/
--- UVA/test.cpp
+++ UVA/test.cpp
@@ -1,77 +1,43 @@
-#include<iostream>
-#include<cstdio>
-#include<cstring>
-#include<cstdlib>
-
+#include<bits/stdc++.h>
 using namespace std;
-char grid[201][201];
-int n;
-int fx[] = {-1,-1, 0, 0,+1,+1};
-int fy[] = {-1, 0,-1,+1, 0,+1};
 
-struct Point{
-    int row;
-    int col;
-};
-
-bool valid(Point point){
-    if(point.col < 0 ||  n <= point.col) return false;
-    if(point.row < 0 ||  n <= point.row) return false;
-    return true;
-}
-
-bool BFS(Point point){
-    int visited[n][n];
-    memset(visited,0,sizeof(visited));
-    Point Q[40000],qPoint, newPoint;
-    int qPos, qsize;
-    qPos = 0;
-    qsize = 1;
-    Q[qPos] = point;
-    visited[point.row][point.col] = 1;
-    while(qPos < qsize){
-        qPoint = Q[qPos++];
-        for(int i = 0; i < 6; i++){
-            newPoint.row = qPoint.row + fx[i];
-            newPoint.col = qPoint.col + fy[i];
-            if(valid(newPoint) && grid[newPoint.row][newPoint.col] == 'b' && visited[newPoint.row][newPoint.col] == 0)
-            {
-                 Q[qsize++] = newPoint;
-                 visited[newPoint.row][newPoint.col] = 1;
-                 if(newPoint.row + 1 == n || newPoint.col + 1 == n)
-                     return true;
-            }
-        }
+int p[26], r[26];
+void init(int n) {
+    while(n >= 0) {
+        p[n] = n, r[n] = 1;
+        n--;
     }
-    return false;
 }
-
-int main(){
-    Point point;
-    bool blackWin = false;
-    int count = 1;
-    while(cin>>n,n){
-        for(int i = 0; i < n; i++)
-            scanf("%s",grid[i]);
-
-        for(int i = 0; i < n; i++){
-            if(grid[0][i] == 'b'){
-                point.row = 0, point.col = i;
-                blackWin = BFS(point);
-                if(blackWin) break;
-            }
-        }
-        cout << count <<" " << (blackWin == true ? "B" : "W") << endl;
-        count++;
+int find(int x) {
+    return p[x] == x ? x : (p[x]=find(p[x]));
+}
+int joint(int x, int y) {
+    x = find(x), y = find(y);
+    if(x != y) {
+        if(r[x] > r[y])
+            r[x] += r[y], p[y] = x;
+        else
+            r[y] += r[x], p[x] = y;
+        return 1;
     }
     return 0;
 }
-
-/*
-4
-bbwb
-wwbw
-bwwb
-wwbb
-
-*/
+int main() {
+    int t;
+    char s[10];
+    scanf("%d ", &t);
+    while(t--) {
+        gets(s);
+        init(s[0]-'A');
+        int ans = s[0]-'A'+1;
+        while(gets(s)) {
+            if(s[0] == '\0')
+                break;
+            ans -= joint(s[0]-'A', s[1]-'A');
+        }
+        printf("%d\n", ans);
+        if(t)
+            puts("");
+    }
+    return 0;
+}
