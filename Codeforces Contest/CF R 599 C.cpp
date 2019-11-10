be713f19e145d3f019ba81c43eea9c3cebf8709c
Rana53 <ranajahid.cseru@gmail.com>
2019-11-10 10:07:22 +0600

    round 599 CF contest problem B2 upsolved

--- Codeforces Contest/CF R 599 C.cpp
+++ Codeforces Contest/CF R 599 C.cpp
@@ -1,37 +1,71 @@
 #include<bits/stdc++.h>
 using namespace std;
-int str1, str2;
+string str1, str2;
 int n;
-int findInLowStr(int i){
-    for(int j = i; j < n; j++){
-        if(str2[j] == str[i]) return j;
+
+int findCharPosition(int i, int lebel, char ch){
+    if(lebel == 1){
+        for(int j = i; j < n ;j++){
+            if (ch == str1[j])
+                return j;
+        }
+    }
+    else{
+        for(int j = i; j < n ;j++){
+            if (ch == str2[j]){
+                return j;
+            }
+
+        }
     }
     return -1;
-}
-
-int findInLowStr(int i){
-    for(int j = i + 1; j < n; j++){
-        if()
-    }
 }
 
 int main(){
     int t;
     cin >> t;
     while(t--){
-        int n;
+        bool solutionPossible = true;
+        int charPos;
         cin >> n;
         cin >> str1;
         cin >> str2;
-        for(int i = 0; i < n - 1; i ++){ //here n - 1 because last time no need to swap
-            if(findInLowStr(i) >= 0){
+        pair <int, int> myPair[n*2];
+        int pairCount = 0;
+        for(int i = 0; i < n; i ++) {
+            if(str1[i] == str2[i]) continue;
+            char tmp;
+            int charPos = findCharPosition(i+1, 2, str2[i]);
+             //cout <<"First" << charPos << endl;
+            if (charPos >= 0) {
+                tmp = str2[charPos];
+                str2[charPos] = str1[i];
+                str1[i] = tmp;
+                myPair[pairCount++] = make_pair(i, charPos);
+            } else {
+                charPos = findCharPosition(i, 1, str2[i]);
+                //cout << charPos << endl;
+                if(charPos < 0){
+                   // cout <<i<< "----------" << str1 << str2 << endl;
+                    solutionPossible = false;
+                    break;
+                }
+                tmp = str1[charPos];
+                str1[charPos] = str2[charPos];
+                str2[charPos] = str1[i];
+                str1[i] = tmp;
+                myPair[pairCount++] = make_pair(charPos, charPos);
+                myPair[pairCount++] = make_pair(i, charPos);
             }
-            else if(findInSameStr[i] >= 0){
 
-            }
-            else{
-                cout << "NO" << endl;
-                return 0;
+        }
+        if (!solutionPossible){
+            cout << "NO" << endl;
+        } else {
+            cout << "YES" << endl;
+            cout << pairCount << endl;
+            for (int i = 0; i < pairCount; i++) {
+                cout << myPair[i].first+1 << " " << myPair[i].second+1 << endl;
             }
         }
     }
