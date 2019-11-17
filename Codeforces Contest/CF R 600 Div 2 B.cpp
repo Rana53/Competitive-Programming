8815c82d2cabfc6ce019909d7480a070b10966cd
Rana53 <ranajahid.cseru@gmail.com>
2019-11-17 23:56:01 +0600

    for positive value it return true

--- Codeforces Contest/CF R 600 Div 2 B.cpp
+++ Codeforces Contest/CF R 600 Div 2 B.cpp
@@ -1,8 +1,16 @@
 #include<bits/stdc++.h>
+#define MX 10000000+7
 using namespace std;
 int main(){
-    cout << "Yes" << endl;
-    cout << "Yes" << endl;
-    
+    int n;
+    cin >> n;
+    cout << MX << endl;
+    int v;
+    bool possible = true, end = false, count = 0, d = -1;
+    for (int i = 0; i < n; i++) {
+        cin >> v;
+        
+    }
+
     return 0;
 }
