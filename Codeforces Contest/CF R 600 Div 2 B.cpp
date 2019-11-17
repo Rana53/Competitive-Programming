c5e24b2d9b858c781899a22d8572a7327d3ab7ab
Rana53 <ranajahid.cseru@gmail.com>
2019-11-18 00:19:09 +0600

    complete code but didnt't solved yet

--- Codeforces Contest/CF R 600 Div 2 B.cpp
+++ Codeforces Contest/CF R 600 Div 2 B.cpp
@@ -1,16 +1,50 @@
 #include<bits/stdc++.h>
 #define MX 10000000+7
+int day[1000000];
 using namespace std;
 int main(){
     int n;
     cin >> n;
     cout << MX << endl;
-    int v;
-    bool possible = true, end = false, count = 0, d = -1;
+    bool possible = true, end = false;
+    int v, count = 0, d = -1;
+    map<int,int> myMp;
     for (int i = 0; i < n; i++) {
         cin >> v;
+        if (!possible) continue;
+        if (v > 0) {
+            if (!count){
+                 d++;
+                 end = false;
+                myMp.clear();
+            }
+            if (!myMp[v]){
+                possible = false;
+            } else {
+                myMp[v] = v;
+                day[d]++;
+                count++;
+            }
+        } else {
+            if (myMp[-1*v] == v) {
+                myMp[-1*v] = 0;
+                count--;
+                day[d]++;
+                if (!count) end = true;
+            } else {
+                possible = false;
+            }
+        }
         
     }
-
+    if (!possible) cout << -1 << endl;
+    else {
+        cout << d + 1 << endl;
+        cout << day[0] << endl;
+        for (int i = 1; i <= d; i++) {
+            cout << " " <<  day[i];
+        }
+    }
     return 0;
 }
+ 
